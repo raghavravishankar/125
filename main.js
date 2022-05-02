6c796b62d20750971a794c94de3c75b8c43f0020
raghavravishankar <92210619+raghavravishankar@users.noreply.github.com>
2022-05-02 21:41:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,42 +1,13 @@
-difference = 0;
-rightWristX = 0;
-leftWristX = 0;
-
-  function setup() {
-  video = createCapture(VIDEO);
-  video.size(550, 500);
-
-  canvas = createCanvas(550, 550);
-  canvas.position(560,150);
-
-  poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose', gotPoses);
-}
-
-function modelLoaded() {
-  console.log('PoseNet Is Initialized!');
-}
-
-
-function gotPoses(results)
+function preload()
 {
-  if(results.length > 0)
-  {
-    console.log(results);
-
-    leftWristX = results[0].pose.leftWrist.x;
-    rightWristX = results[0].pose.rightWrist.x;
-    difference = floor(leftWristX - rightWristX);
-
-    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
-  }
+    song = loadSound("Satisfy Imran Khan.mp3");
+    song = loadSound("Tera Baap Aaya.mp3");
 }
 
-function draw() {
-background('#6C91C2');
-
-  document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
-textSize(difference);
-fill('#FFE787');
-text('Peter', 50, 400);
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+    
+    video = createCapture(VIDEO);
+    video.hide();
 }
